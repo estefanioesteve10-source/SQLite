6b8ae7f8e426449ebb71e15ae5633bc3ceb87875
Estefanio Fernando <estefanioesteve10@gmail.com>
2026-02-12 22:48:58 +0000

    commit

---
+++ Banco de Dados/conexao.py
@@ -0,0 +1,26 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_PATH = Path().parent
+
+conexao = sqlite3.connect(ROOT_PATH / "meu_banco.db")
+cursor = conexao.cursor()
+
+def criar_tabela(conexao, cursor):
+    cursor.execute(
+        "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(50) )"
+    )
+    conexao.commit()
+
+
+def inserir_registro(conexao, cursor, nome, email):
+    data = (nome, email)
+    cursor.execute('INSERT INTO clientes (nome, email) VALUES (?,?);', data)
+    conexao.commit()
+
+def atualizar_registro(conexao, cursor, nome, email, id):
+    data = (nome, email, id)
+    cursor.execute('UPDATE clientes SET nome=?, email=? WHERE id=?;', data)
+    conexao.commit()
+
+atualizar_registro(conexao, cursor, 'Adilson', 'adilson@gmail.com',1)
